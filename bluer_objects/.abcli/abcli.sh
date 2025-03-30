9772b792f9554a382d3e7e18fdaa9ba0313bba99
kamangir <arash@kamangir.net>
2025-03-30 13:55:44 -0700

    wip - kamangir/bolt#746

--- bluer_objects/.abcli/abcli.sh
+++ bluer_objects/.abcli/abcli.sh
@@ -2,8 +2,8 @@
 
 abcli_source_caller_suffix_path /tests
 
-abcli_env_dot_load \
+bluer_ai_env_dot_load \
     caller,plugin=bluer_objects,suffix=/../..
 
-abcli_env_dot_load \
+bluer_ai_env_dot_load \
     caller,filename=config.env,suffix=/..
