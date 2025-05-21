a0ccedccb26faaf2443bdcb0bc250e2ad86e2b7a
kamangir <arash@kamangir.net>
2025-05-21 11:23:41 +0330

    ++ - kamangir/bolt#746

--- bluer_objects/tests/test_file_download.py
+++ bluer_objects/tests/test_file_download.py
@@ -12,10 +12,14 @@ from bluer_objects import objects, file
 def test_file_download(url: str):
     object_name = objects.unique_object("test_file_download")
 
+    filename = objects.path_of(
+        object_name=object_name,
+        filename=url.split("/")[-1],
+    )
+
     assert file.download(
         url=url,
-        filename=objects.path_of(
-            object_name=object_name,
-            filename=url.split("/")[-1],
-        ),
+        filename=filename,
     )
+
+    assert file.exists(filename)
