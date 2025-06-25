0bacb7923f711e77cd8d7c5c0358a5f2bb68be4b
kamangir <arash@kamangir.net>
2025-06-25 13:55:14 +0330

    ++ - kamangir/bolt#746

--- bluer_objects/url/functions.py
+++ bluer_objects/url/functions.py
@@ -6,9 +6,6 @@ from bluer_objects.logger import logger
 def is_accessible(url) -> bool:
     try:
         response = requests.get(url)
-        if response.status_code == 200:
-            return True
-        else:
-            return False
+        return response.status_code == 200
     except:
         return False
