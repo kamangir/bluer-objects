d01ac0950637cb0d5c0bcf7ab9dc9eca9cf61b48
kamangir <arash@kamangir.net>
2025-07-10 16:16:14 +0330

    ++ - kamangir/bolt#746

--- bluer_objects/README/utils.py
+++ bluer_objects/README/utils.py
@@ -86,6 +86,9 @@ def process_envs(template_line: str) -> str:
         env_name = template_line.split("env:::", 1)[1]
         if " " in env_name:
             env_name = env_name.split(" ", 1)[0]
+        else:
+            if ":::" in env_name:
+                env_name = env_name.split(":::", 1)[0]
 
         env_value = get_env(env_name)
 
