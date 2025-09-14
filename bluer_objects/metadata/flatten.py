d352ba4d00b13751dc3004396396dd0a5fdbfadb
kamangir <arash@kamangir.net>
2025-09-14 14:09:59 +0330

    ++ - kamangir/bolt#746

--- bluer_objects/metadata/flatten.py
+++ bluer_objects/metadata/flatten.py
@@ -5,18 +5,23 @@ import numpy as np
 def flatten(obj: Any) -> Any:
     if isinstance(obj, dict):
         return {k: flatten(v) for k, v in obj.items()}
-    elif isinstance(obj, list):
+
+    if isinstance(obj, list):
         return [flatten(x) for x in obj]
-    elif isinstance(obj, tuple):
+
+    if isinstance(obj, tuple):
         return tuple(flatten(x) for x in obj)
-    elif isinstance(obj, np.ndarray):
+
+    if isinstance(obj, np.ndarray):
         return obj.tolist()
-    elif hasattr(obj, "__dict__"):
+
+    if hasattr(obj, "__dict__"):
         return flatten(vars(obj))
-    elif isinstance(obj, (int, float, str)):
+
+    if isinstance(obj, (int, float, str)):
         return obj
-    else:
-        try:
-            return str(obj)
-        except:
-            return obj.__class__.__name__
+
+    try:
+        return str(obj)
+    except:
+        return obj.__class__.__name__
