1f7c093a7209c1384e26695f4a62eb9d13a978d6
kamangir <arash@kamangir.net>
2025-12-18 10:49:31 +0330

    pipeline fix, hopefully - kamangir/bolt#746

--- bluer_objects/.abcli/tests/storage_clear.sh
+++ bluer_objects/.abcli/tests/storage_clear.sh
@@ -3,10 +3,6 @@
 function test_bluer_objects_storage_clear() {
     local options=$1
 
-    bluer_ai_storage_clear
-    [[ $? -ne 0 ]] && return 1
-    bluer_ai_hr
-
     bluer_ai_storage_clear cloud
     [[ $? -ne 0 ]] && return 1
     bluer_ai_hr
--- bluer_objects/__init__.py
+++ bluer_objects/__init__.py
@@ -4,7 +4,7 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} Object management in Bash."
 
-VERSION = "6.365.1"
+VERSION = "6.366.1"
 
 REPO_NAME = "bluer-objects"
 
