6aee77fba36a7387b2a308e12d5299449a2c7e83
kamangir <arash@kamangir.net>
2026-01-05 18:36:57 +0330

    ++

--- bluer_objects/tests/test_mlflow_serverless.py
+++ bluer_objects/tests/test_mlflow_serverless.py
@@ -3,23 +3,23 @@ import pytest
 from bluer_options.options import Options
 
 from bluer_objects.objects import unique_object
-from bluer_objects.mlflow.serverless import write
+from bluer_objects.mlflow.serverless import get_tags, set_tags
 
 
 @pytest.mark.parametrize(
     ["tags_str"],
     [["x=1,y=2,z=3"]],
 )
-def test_mlflow_storage_tag_set_get(tags_str: str):
-    object_name = unique_object("test_mlflow_storage_tag_set_get")
+def test_mlflow_serverless_tags_set_get(tags_str: str):
+    object_name = unique_object("test_mlflow_serverless_tags_set_get")
 
-    assert write.set_tags(
+    assert set_tags(
         object_name,
         tags_str,
         log=False,
     )
 
-    success, tags_read = write.get_tags(object_name)
+    success, tags_read = get_tags(object_name)
     assert success
 
     tags_option = Options(tags_str)
