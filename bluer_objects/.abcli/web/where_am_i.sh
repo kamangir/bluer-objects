798bbe43b876f59a4636c5029854be803ed6e9ad
kamangir <arash@kamangir.net>
2026-01-10 13:45:58 +0330

    ++

--- bluer_objects/.abcli/web/where_am_i.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-function bluer_objects_web_where_am_i() {
-    curl ifconfig.co/json
-}
--- bluer_objects/__init__.py
+++ bluer_objects/__init__.py
@@ -4,7 +4,7 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} Object management in Bash."
 
-VERSION = "6.425.1"
+VERSION = "6.426.1"
 
 REPO_NAME = "bluer-objects"
 
