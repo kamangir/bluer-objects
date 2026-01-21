28797c19011d7ab8a3e7f2825a0e6af55feb5eb4
kamangir <arash@kamangir.net>
2026-01-21 11:39:24 +0330

    ++

--- bluer_objects/.abcli/tests/pdf.sh
+++ bluer_objects/.abcli/tests/pdf.sh
@@ -23,6 +23,10 @@ function test_bluer_objects_pdf_convert_inline() {
 function test_bluer_objects_pdf_convert() {
     local options=$1
 
+    local do_install=1
+    [[ "$abcli_is_mac" == true ]] &&
+        do_install=0
+
     local object_name=test_bluer_objects_pdf_convert-2025-11-30-ci0cd7
 
     bluer_ai_eval ,$options \
